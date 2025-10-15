c901bb8e3d474e1b88beb4fcd664e2a58314ea91
Naveen Kumar <naveenku@jfrog.com>
2025-10-15 10:18:26 +0530

    Feature/rteco 456 implement groups api amnd versions api (#279)

--- lifecycle/commands/releaseBundleSearchGroups.go
+++ lifecycle/commands/releaseBundleSearchGroups.go
@@ -1,8 +1,16 @@
 package commands
 
 import (
+	"encoding/json"
+	"errors"
+	"github.com/jedib0t/go-pretty/v6/text"
+	"github.com/jfrog/jfrog-cli-artifactory/stats"
 	rtUtils "github.com/jfrog/jfrog-cli-core/v2/artifactory/utils"
 	"github.com/jfrog/jfrog-cli-core/v2/utils/config"
+	"github.com/jfrog/jfrog-cli-core/v2/utils/coreutils"
+	"github.com/jfrog/jfrog-client-go/lifecycle/services"
+	clientutils "github.com/jfrog/jfrog-client-go/utils"
+	"github.com/jfrog/jfrog-client-go/utils/log"
 )
 
 type SearchGroupCommand struct {
@@ -67,9 +75,51 @@ func (sgc *SearchGroupCommand) Run() error {
 	if err != nil {
 		return err
 	}
-	_, err = lcServicesManager.ReleaseBundlesSearchGroup()
+	queryParameters := services.GetSearchOptionalQueryParams{
+		Offset:   sgc.offset,
+		Limit:    sgc.limit,
+		FilterBy: sgc.filterBy,
+		OrderBy:  sgc.orderBy,
+		OrderAsc: sgc.orderAsc,
+	}
+	searchGroupResponse, err := lcServicesManager.ReleaseBundlesSearchGroup(queryParameters)
 	if err != nil {
 		return err
+	}
+	if sgc.format == "json" {
+		content, err := json.Marshal(searchGroupResponse)
+		if err != nil {
+			return err
+		}
+		log.Output(clientutils.IndentJson(content))
+		return nil
+	} else {
+		return printReleaseBundleSearchGroupTable(searchGroupResponse)
+	}
+}
+
+func printReleaseBundleSearchGroupTable(searchGroupResponse services.ReleaseBundlesGroupResponse) error {
+	displayLimit := 10
+	loopRange := len(searchGroupResponse.ReleaseBundleSearchGroup)
+	if loopRange > displayLimit {
+		loopRange = displayLimit
+	}
+	actualCount := len(searchGroupResponse.ReleaseBundleSearchGroup)
+	tableData := []stats.TableRow{{Metric: "Name", Value: "Latest Version"}}
+	for i := 0; i < loopRange; i++ {
+		rbDetails := searchGroupResponse.ReleaseBundleSearchGroup[i]
+		tableData = append(tableData, stats.TableRow{
+			Metric: text.FgHiBlue.Sprint(rbDetails.ReleaseBundleName),
+			Value:  text.FgHiGreen.Sprint(rbDetails.ReleaseBundleVersionLatest),
+		})
+	}
+	footer := ""
+	if actualCount > displayLimit {
+		footer = text.FgYellow.Sprintf("\n...and %d more release bundle names. Refer JSON output format for complete list.", actualCount-displayLimit)
+	}
+	err := coreutils.PrintTableWithBorderless(tableData, text.FgCyan.Sprint("Release Bundles Details"), footer, "No Release Bundle Found", false)
+	if err != nil {
+		return errors.New("failed to print ReleaseBundlesSearchGroup table")
 	}
 	return nil
 }
